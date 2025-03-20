b02033938e8a814e398c0b252c079f5dfb563e43
weslley vicente <weslleyvicente373@gmail.com>
2025-03-20 10:53:12 -0300

    Add files via upload

--- cachorro.py
+++ cachorro.py
@@ -1,10 +1,11 @@
 class Cachorro:
-    def __init__(self, nome, raca, comida):
+    def __init__(self, nome, raca, comida, energia):
         self.nome = nome
         self.raca = raca
         self.comida = comida
         self.acordado = False
         self.feliz = False
+        self.energia = energia
 
     def __str__(self):
         return f"Nome:  {self.nome}, Raça: {self.raca}, Comida: {self.comida}, Acordado: {self.acordado}, Feliz: {self.feliz}"
@@ -29,8 +30,25 @@ class Cachorro:
            self.feliz = False
            print(f"Você ignorou {self.nome} e ficou triste")
 
-cachorro1 = Cachorro("Kiara", "SRD", 20)
+    def brincar(self):
+        if self.energia >= 20:
+            self.energia -= 20
+            self.feliz = True
+            print(f"{self.nome} Brincou e está muito FELIZ!!!Energia restante: {self.energia}")
+        else:
+            print(f"{self.nome} está muito Cansado e não tem Energia suficiente para Brincar!")
+    
+
+
+cachorro1 = Cachorro("Kiara", "SRD", 20, 100)
+print(cachorro1)
+
+cachorro1.acordar()
+cachorro1.dormir()
 cachorro1.passear()
+cachorro1.ignorar()
+cachorro1.brincar()
+
 
 
     
